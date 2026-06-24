afc7d5445dcca16fad97423abe871fd4bbd21bf4
Sandeep Kumar <sksandeepds37@gmail.com>
2026-06-24 18:12:19 +0530

    Added:Company schema

--- src/config/db.js
+++ src/config/db.js
@@ -18,6 +18,28 @@ export async function initialiseDatabaseTable() {
       updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     );
   `);
-
+  await dbPool.query(`
+      CREATE TABLE IF NOT EXISTS companies(
+      id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+      user_id VARCHAR(255) NOT NULL,
+      company_name VARCHAR(150) NOT NULL,
+      address TEXT,
+      city VARCHAR(80),
+      state VARCHAR(80),
+      country VARCHAR(80),
+      pincode VARCHAR(15),
+      gst_number VARCHAR(20),
+      pan_number VARCHAR(20),
+      financial_year_start DATE,
+      financial_year_end DATE,
+      phone VARCHAR(20),
+      email VARCHAR(120),
+      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+      updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+      FOREIGN KEY(user_id)
+      REFERENCES users(id)
+      ON DELETE CASCADE
+      )
+    `);
   console.log("Database tables initialized successfully");
 }
